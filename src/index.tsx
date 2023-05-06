59099a0c18c4c06b9eacedccc695c33491dee2bd
mirislamus <mirislamus@gmail.com>
2023-05-06 14:25:29 +0500

    feat: update router

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
