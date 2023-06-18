c3ea50542ecdb5e7220c59020770188677bc535e
Mirislamus <mirislamus@gmail.com>
2023-06-18 13:16:42 +0500

    feat: add typography

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,7 +1,9 @@
 import { extendTheme } from '@chakra-ui/react';
 import breakpoints from './breakpoints';
+import typography from './typography';
 
 const theme = {
+  ...typography,
   components: {},
   config: {
     useSystemColorMode: false,
