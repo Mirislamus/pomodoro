374ec93c0748d5f4bc1c17efbbf36c0f4804553c
Mirislamus <mirislamus@gmail.com>
2023-12-11 18:11:10 +0500

    feat: add textStyles

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,9 +1,19 @@
 import { extendTheme } from '@chakra-ui/react';
 import breakpoints from './breakpoints';
 import typography from './typography';
+import textStyles from './typography/text-styles';
 
 const theme = {
   ...typography,
+  textStyles,
+  styles: {
+    global: {
+      body: {
+        textStyles: 'text.md',
+        letterSpacing: 'normal',
+      },
+    },
+  },
   components: {},
   config: {
     useSystemColorMode: false,
---
+++ src/theme/typography/text-styles.ts
@@ -0,0 +1,43 @@
+const textStyles = {
+  title: {
+    lg: {
+      fontSize: {
+        base: '70px',
+        md: '120px',
+      },
+      lineHeight: {
+        base: '85px',
+        md: '145px',
+      },
+      fontWeight: 'medium',
+    },
+    sm: {
+      fontSize: '14px',
+      lineHeight: '17px',
+    },
+  },
+  text: {
+    xl: {
+      fontSize: '18px',
+      lineHeight: '22px',
+      fontWeight: 'medium',
+    },
+    lg: {
+      fontSize: '15px',
+      lineHeight: '18px',
+      fontWeight: 'regular',
+    },
+    md: {
+      fontSize: '14px',
+      lineHeight: '17px',
+      fontWeight: 'semibold',
+    },
+    sm: {
+      fontSize: '13px',
+      lineHeight: '18px',
+      fontWeight: 'regular',
+    },
+  },
+};
+
+export default textStyles;
