9813e2e119bd0aa6973e0d9c0312dce438044ce4
mirislamus <42132184+Mirislamus@users.noreply.github.com>
2023-12-12 12:48:55 +0500

    feat: add colors & tokens (#18)

---
+++ src/theme/typography/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+  red: '#ED4455',
+  black: {
+    1: '#222222',
+    2: '#262324',
+    alpha: 'rgba(34, 34, 34, 0.5)',
+  },
+  grey: {
+    1: '#DEDEDE',
+    2: '#EDEDED',
+    3: '#999999',
+    4: '#4E4E4E',
+    5: '#434343',
+  },
+};
+
+export default colors;
---
+++ src/theme/typography/semantic-tokens.ts
@@ -0,0 +1,30 @@
+const semanticTokens = {
+  colors: {
+    primary: {
+      _light: 'black.1',
+      _dark: 'white',
+    },
+    secondary: {
+      _light: 'grey.1',
+      _dark: 'grey.4',
+    },
+    tertiary: {
+      _light: 'grey.2',
+      _dark: 'grey.5',
+    },
+    additionaly: {
+      default: 'grey.3',
+    },
+    background: {
+      primary: {
+        _light: 'white',
+        _dark: 'black',
+      },
+      modal: {
+        default: 'black.alpha',
+      },
+    },
+  },
+};
+
+export default semanticTokens;
