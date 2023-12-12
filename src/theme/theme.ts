763580d4ffcbc9bf88c102bf8197e0390274a363
Mirislamus <mirislamus@gmail.com>
2023-12-12 12:47:21 +0500

    feat: add colors & tokens

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,9 +2,13 @@ import { extendTheme } from '@chakra-ui/react';
 import breakpoints from './breakpoints';
 import typography from './typography';
 import textStyles from './typography/text-styles';
+import colors from './typography/colors';
+import semanticTokens from './typography/semantic-tokens';
 
 const theme = {
   ...typography,
+  colors,
+  semanticTokens,
   textStyles,
   styles: {
     global: {
