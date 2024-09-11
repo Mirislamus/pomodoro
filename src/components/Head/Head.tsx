8617b3fad39f6023db7eb57659598ae0db6112bd
Mirislamus <mirislamus@gmail.com>
2024-09-11 17:16:05 +0500

    Trigger

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -8,13 +8,10 @@ const Head: FC = () => {
       <Helmet>
         <title>{meta.title}</title>
         <meta name="description" content={meta.description} />
-
         <link rel="icon" type="image/svg+xml" href="/images/favicons/favicon.svg" />
         <link rel="icon" type="image/x-icon" href="/images/favicons/favicon.ico" />
-
         <meta name="msapplication-TileColor" content={meta.color} />
         <meta name="theme-color" content={meta.color} />
-
         <meta property="og:url" content={meta.url} />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={meta.title} />
@@ -22,7 +19,6 @@ const Head: FC = () => {
         <meta property="og:image" content={`${meta.url}/images/opengraph.jpg`} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content={meta.url} />
         <meta name="twitter:title" content={meta.title} />
