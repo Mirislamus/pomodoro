e5fd8760b646c9b3c0c8e3280c8fc40c3a9f96a6
Mirislamus <mirislamus@gmail.com>
2024-10-19 15:01:43 +0500

    feat: update description

--- src/data/meta.ts
+++ src/data/meta.ts
@@ -1,7 +1,7 @@
 const meta = {
   title: 'Pomotomo Focus Timer',
   description:
-    'Pomotomo is your personal Pomodoro timer to increase productivity. Work efficiently by dividing tasks into short sessions with breaks. Ideal for focusing and completing tasks.',
+    'Pomotomo is your personal pomodoro timer focus to increase productivity. Work efficiently by dividing tasks into short sessions with breaks. Ideal for focusing and completing tasks.',
   color: '#ED4455',
   short_name: 'Pomotomo',
   url: 'https://pomotomo.uz',
